211d86e5e1b61847c39a18e686d0857e3bd8db2f
Francisco <fnaveira11@gmail.com>
2021-06-04 22:20:10 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,7 @@
-class Application
+require 'pry'
 
+class Application
+    
     @@items =[]
     
     def call(env)
@@ -8,8 +10,9 @@ class Application
        
        if req.path.match(/items/)
             item_name = req.path.split("/items/").last 
-            if @@items.include? (item_name) 
-                item = @@items.find{|i| i.name == item_name}
+            item = @@items.find{|i| i.name == item_name}
+            
+            if @@items.include? (item) 
                 resp.write item.price
             else
                 resp.write "Item not found"
@@ -22,21 +25,5 @@ class Application
     
         resp.finish
     end
-
-    # def call(env)
-    #   resp = Rack::Response.new
-    #   req = Rack::Request.new(env)
-
-    #   if req.path.match(/items/)
-    #     item_name = req.path.split("/items/").last 
-    #     item = @@items.find{|i| i.name == item_name}
-    #     resp.write item.price
-    #   else
-    #     resp.write "Route not found"
-    #     resp.status = 404
-    #   end
-  
-    #   resp.finish
-    # end
 
 end 
